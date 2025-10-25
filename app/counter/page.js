78a3c1773e736f7f7ff5e35b45f19c8052fe24d7
Warrick Tsui <warricktsui1226@gmail.com>
2025-10-25 14:00:09 -0400

    Comments on master

--- app/counter/page.js
+++ app/counter/page.js
@@ -1,14 +1,18 @@
 "use client";
 
 export default function Counter() {
+    // Initialize variables for "count"
+
+    // Call setCount here --- create the function to update the count!
 
   return (
-    <div>
+    <div> {/* Set the screen to pink and justify everything in the center! */}
+      {/* Link to your back butotn here  */}
       <div>
-        <h1>This is a Counter!!!</h1>
+        <h1>This is a Counter!!!</h1> {/* Make this button LARGER! */}
         <h1></h1>
 
-        <div>
+        <div> {/* When you click these buttons, adjust the "count" variables accordingly!*/}
           <button>Increase</button>
           <button>Decrease</button>
           <button>Reset</button>
